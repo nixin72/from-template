932624271ba0819ad382d15099739766c3acc02c
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2020-08-28 16:53:29 +0100

    Add macosx support (#4)
    * Add files via upload
    
    * add macosx support
    
    * Update main.rkt
    
    * Delete from-template-macosx.sh
    
    * Update main.rkt

--- main.rkt
+++ main.rkt
@@ -7,10 +7,16 @@
 
 (define-runtime-path windows-script "from-template.bat")
 
+(define-runtime-path macosx-script "from-template.sh")
+
 (define [clone-repo repo-name dir-name]
-  (if (eq? (system-type 'os) 'windows)
-    (system (string-append (path->string windows-script) " " repo-name " " dir-name))
-    (system (string-append "bash ./from-template.sh " repo-name " " dir-name))))
+  (case (system-type 'os)
+    [(unix)
+     (system (string-append "bash ./from-template.sh " repo-name " " dir-name))]
+    [(macosx)
+     (system (string-append "bash " (path->string macosx-script) " " repo-name " " dir-name))]
+    [(windows)
+     (system (string-append (path->string windows-script) " " repo-name " " dir-name))]))
 
 (match (current-command-line-arguments)
   [(vector repo dir)
@@ -20,3 +26,4 @@
   [(vector)
    (error "Need to supply a name of a template. Checkout https://github.com/racket-templates for a list of available templates.")]
   [_ (error "Too many arguments supplied. Command should be in form `raco from-template <template-name> <dir-name>`")])
+
