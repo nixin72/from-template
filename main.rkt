af941ce9025310be23dce3ffdde8a9c4b5c3189a
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2020-09-15 12:52:10 +0100

    Update main.rkt

--- main.rkt
+++ main.rkt
@@ -27,3 +27,4 @@
    (error "Need to supply a name of a template. Checkout https://github.com/racket-templates for a list of available templates.")]
   [_ (error "Too many arguments supplied. Command should be in form `raco from-template <template-name> <dir-name>`")])
 
+(module test racket/base)
