99a9cd246b8e623e973cdb8544cbd5cab4741cb1
Philip Dumaresq <phdumaresq@protonmail.com>
2020-09-24 21:13:37 -0400

    Added proper command line argument parser

--- main.rkt
+++ main.rkt
@@ -2,6 +2,7 @@
 
 (require racket/system
          racket/match
+         racket/cmdline
          racket/path
          racket/runtime-path)
 
@@ -18,13 +19,35 @@
     [(windows)
      (system (string-append (path->string windows-script) " " repo-name " " dir-name))]))
 
-(match (current-command-line-arguments)
-  [(vector repo dir)
-   (clone-repo repo dir)]
-  [(vector repo)
-   (clone-repo repo repo)]
-  [(vector)
-   (error "Need to supply a name of a template. Checkout https://github.com/racket-templates for a list of available templates.")]
-  [_ (error "Too many arguments supplied. Command should be in form `raco from-template <template-name> <dir-name>`")])
+(define too-few-arguments-error
+  (string-append
+   "Need to supply a name of a template. "
+   "Checkout https://github.com/racket-templates for a list of available templates."))
+(define too-many-arguments-error
+  (string-append
+   "Too many arguments supplied. "
+   "Command should be in form `raco from-template <template-name> <dir-name>`"))
+
+(define interactive? (make-parameter #f))
+(define template (make-parameter null))
+(define output-dir (make-parameter null))
+
+(define cli-args
+  (command-line
+   #:program "from-template"
+   #:once-each
+   [("-i" "--interactive") "Allows you to modify the instantiated directory interactively"
+                           (interactive? #t)]
+   #:args args
+   (let ([success?]
+         (match args
+          [(list repo dir) ; Clone repo using new directory name
+           (clone-repo repo dir)]
+          [(list repo) ; Clone repo without changing output location
+           (clone-repo repo repo)]
+          ;; Errors
+          [(list) (error too-few-arguments-error)]
+          [_ (error too-many-arguments-error)]))
+     (println interactive?))))
 
 (module test racket/base)
